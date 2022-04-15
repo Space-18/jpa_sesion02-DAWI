06074b168fe7d50c30e16a167df0d80df2b03180
Nicolas <nicolasrodrigo22@outlook.es>
2022-04-15 00:21:14 -0500

    nuevo frm login

--- src/main/java/app/Login.java
+++ src/main/java/app/Login.java
@@ -9,23 +9,6 @@ import model.Usuario;
 
 public class Login {
 	public static void main(String[] args) {
-		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
-		EntityManager em = fabrica.createEntityManager();
 		
-		//select * from tb_usuarios where idTipo = 1
-		Query consulta = em.createNativeQuery("{call usp_validaAcceso (?,?)}",Usuario.class);
-		consulta.setParameter(1,"U001@gmail.com");
-		consulta.setParameter(2,"10001");
-		
-		Usuario user = (Usuario)consulta.getResultList();
-		
-		if (user == null) {
-			System.out.println("Usuario no existe");
-		}else {
-			System.out.println("Hola " + user.getNombre() + "Bienvenido!!!");
-			System.out.println("Tus datos son: " + user);
-		}
-		
-		em.close();
 	}
 }
